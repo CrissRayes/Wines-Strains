bcf4fa52661487118e36d25db34c7c67916bbab3
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2022-06-01 22:28:43 -0400

    Wine Strain Relationship done

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_02_022349) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_02_022642) do
   create_table "strains", force: :cascade do |t|
     t.string "name"
     t.decimal "percentage"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "strains_wines", id: false, force: :cascade do |t|
+    t.integer "wine_id", null: false
+    t.integer "strain_id", null: false
+    t.index ["strain_id", "wine_id"], name: "index_strains_wines_on_strain_id_and_wine_id"
+    t.index ["wine_id", "strain_id"], name: "index_strains_wines_on_wine_id_and_strain_id"
   end
 
   create_table "wines", force: :cascade do |t|
